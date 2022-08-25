e8bfcde419eecae74bf9fdd08267afae72e31aaf
ibrahmozdemir1 <ibrahim.ozdemir@bil.omu.edu.tr>
2022-08-25 15:10:59 +0300

    DataAccessLayer katmanındaki bağımlılıkla tanımlandı. Bussiness Layer'a giriş yapıldı.

---
+++ BusinessLayer/Concrete/CategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        EfCategoryRepository categoryRepository;
+
+        public CategoryManager()
+        {
+            categoryRepository = new EfCategoryRepository();
+        }
+        public void CategoryAdd(Category category)
+        {
+            categoryRepository.Insert(category);
+        }
+
+        public void CategoryRemove(Category category)
+        {
+            categoryRepository.Delete(category);
+        }
+
+        public void CategoryUpdate(Category category)
+        {
+            categoryRepository.Update(category);
+        }
+
+        public Category GetById(int id)
+        {
+            return categoryRepository.GetById(id);
+        }
+
+        public List<Category> getList()
+        {
+            return categoryRepository.GetListAll();
+        }
+    }
+}
